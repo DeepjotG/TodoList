22c8df8b59d7b0fee207e28e1e2e424992e055cc
DeepjotG <deepjotgrewal50@gmail.com>
2025-12-20 00:12:23 -0800

    Done

--- client/src/App.js
+++ client/src/App.js
@@ -4,16 +4,17 @@ import "./App.css";
 //components
 
 import InputTodo from "./components/InputToDo";
+import ListTodos from "./components/ListToDo";
 
 function App() {
-  return (
-    <Fragment>
-      <div className="container">
-      <InputTodo />
-
-      </div>
-    </Fragment>
-  );
+	return (
+		<Fragment>
+			<div className="container">
+				<InputTodo />
+				<ListTodos />
+			</div>
+		</Fragment>
+	);
 }
 
 export default App;
--- client/src/components/InputToDo.js
+++ client/src/components/InputToDo.js
@@ -10,12 +10,11 @@ const InputTodo = () => {
         try {
           const body = {description};
           const response = await fetch("http://localhost:5000/todos", {
-            method: "POST",
-            headers: {"Content-Type": "application/json"},
-            body: JSON.stringify(body)
-          });
-
-          console.log(response);
+						method: "POST",
+						headers: { "Content-Type": "application/json" },
+						body: JSON.stringify(body),
+					});
+					window.location = "/";
         } catch (err) {
             console.log(err.message)
             
